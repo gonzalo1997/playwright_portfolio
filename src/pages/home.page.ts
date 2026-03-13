27833621e8d39487ed9b6a67d4e3ba0d967bda79
gonzalo1997 <gonza.magallanes1997@gmail.com>
2026-03-13 11:20:05 -0300

    creation home.page.ts

---
+++ src/pages/home.page.ts
@@ -0,0 +1,23 @@
+import { Page, Locator } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+
+  // Home page locators
+  readonly homePageImage: Locator;
+  readonly signupLoginLink: Locator;
+
+  
+  constructor(page: Page) {
+    this.page = page;
+
+    // Initialize locators
+    this.homePageImage = this.page.locator('img[alt="Website for automation practice"]');
+    this.signupLoginLink = this.page.locator('a[href="/login"]');
+  }
+
+  // Optional: Add methods for common actions, e.g.,
+  async gotoHome() {
+    await this.page.goto('https://automationexercise.com');
+  }
+}
